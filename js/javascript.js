10e60b32976e27c50f07e9087f706eacb184fe77
EDG182023 <63550664+EDG182023@users.noreply.github.com>
2022-06-25 00:48:34 -0300

    Codigo Js carrete
    Chicos, les dejo el código que cambia automática mente las imágenes de carrete.

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,22 @@
-if (document.readyState === 'loading') {
+//Codigo js para el cambio automatico de las imagenes
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Cambia la imagen cada 5 seg
+}
+
+//Codigo viejo 
+
+/*if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', SetFunctions());
   } else {
     SetFunctions();
@@ -22,4 +40,4 @@ function sendForm() {
 function toggleStudents() {
     const info = document.getElementById("studentsInformation")
     info.classList.toggle("hidden");
-}
+}*/
